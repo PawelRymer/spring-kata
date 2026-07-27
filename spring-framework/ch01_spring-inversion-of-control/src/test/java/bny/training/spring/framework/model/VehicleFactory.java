0d692f07829eff82ebdbe4f889ba5436959de821
PawelRymer <pawelrymer+github@gmail.com>
2026-07-27 15:24:25 +0200

    Complete lab01 "Inversion of Control"

--- spring-framework/ch01_spring-inversion-of-control/src/test/java/bny/training/spring/framework/model/VehicleFactory.java
+++ spring-framework/ch01_spring-inversion-of-control/src/test/java/bny/training/spring/framework/model/VehicleFactory.java
@@ -20,14 +20,11 @@ package bny.training.spring.framework.model;
 public class VehicleFactory {
 
     public static Vehicle getStaticVehicle() {
-
-        //TODO: fix this.
-        return null;
+        return new Vehicle("truck", 2016);
     }
 
     public Vehicle getVehicle() {
 
-        //TODO: fix this.
-        return null;
+        return new Vehicle("bike", 2015);
     }
 }
